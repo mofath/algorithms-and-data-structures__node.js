b1f49ce6f37012ded7f041d68af062785ab16351
mofath <mohamed.elgaedi@gmail.com>
2021-01-20 18:59:40 +0200

    Adjusted loop starting iteration

--- 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
+++ 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
@@ -1,6 +1,7 @@
 function bubleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 1; j < arr.length; j++) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(i, j);
       if (arr[j] > arr[j+1]) {
         let temp = arr[j];
         arr[j] = arr[j+1];
