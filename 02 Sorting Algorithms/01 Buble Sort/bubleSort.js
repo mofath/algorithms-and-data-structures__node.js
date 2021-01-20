c2ecc8cb383337c9c1c4ff025624c151f74a1d46
mofath <mohamed.elgaedi@gmail.com>
2021-01-20 19:23:20 +0200

    Code cleanup

--- 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
+++ 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
@@ -1,12 +1,10 @@
+const swap = (arr, idx1, idx2) =>
+  ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
 function bubleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length; j++) {
-      console.log(i, j);
-      if (arr[j] > arr[j+1]) {
-        let temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-      }
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
     }
   }
   return arr;
