fabfb691f2acb28621cd4bc3280731e01730a0c8
mofath <mohamed.elgaedi@gmail.com>
2021-01-21 19:30:56 +0200

    Enhanced algorithm, sort only if not sorted

--- 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
+++ 02 Sorting Algorithms/01 Buble Sort/bubleSort.js
@@ -2,9 +2,15 @@ const swap = (arr, idx1, idx2) =>
   ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
 
 function bubleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
+  let isSwapped = true;
+  while (isSwapped) {
+    isSwapped = false;
     for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+      if (arr[j] > arr[j + 1]) {
+        console.log("here");
+        swap(arr, j, j + 1);
+        isSwapped = true;
+      }
     }
   }
   return arr;
