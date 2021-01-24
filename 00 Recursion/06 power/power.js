b0836ac5cb58f7eb279615238a6662bb9df6e3bb
mofath <mohamed.elgaedi@gmail.com>
2021-01-24 04:30:28 +0200

    Enhance time complexity for calculating power recursively

--- 00 Recursion/06 power/power.js
+++ 00 Recursion/06 power/power.js
@@ -1,6 +1,16 @@
+// function power(base, exponent) {
+//   if (exponent === 0) return 1;
+//   return base * power(base, exponent - 1);
+// }
+
+// enhanced version
 function power(base, exponent) {
   if (exponent === 0) return 1;
-  return base * power(base, exponent - 1);
+  if (exponent === 1) return base;
+  if (exponent % 2 === 0) {
+    const res = power(base, exponent / 2);
+    return res * res;
+  } else return base * power(base, exponent - 1);
 }
 
 module.exports = power;
