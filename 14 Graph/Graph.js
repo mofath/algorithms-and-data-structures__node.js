044fe3a7bf34f5fcf26c215df7a7d1160b7a8389
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-09-17 17:17:46 +0200

    [graph]: added dfs recursive

--- 14 Graph/Graph.js
+++ 14 Graph/Graph.js
@@ -3,7 +3,7 @@ class Graph {
     this.adjacencyList = {};
   }
 
-  addVertix(vertex) {
+  addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
@@ -25,4 +25,48 @@ class Graph {
 
     delete this.adjacencyList[vertex];
   }
+
+  DFSRecursive(start) {
+    const visited = {};
+    const result = [];
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) return dfs(neighbor);
+      });
+    })(start);
+
+    return result;
+  }
 }
+
+let g = new Graph();
+
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+g.DFSRecursive('A');
+
+console.log(g.DFSRecursive('A'));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
